99a4cd7132efb5257dc564868095a2fd160fa2ba
Marcin Galeziowski <marcin_galeziowski@epam.com>
2020-10-28 14:07:48 +0100

    Unit Tests

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-# AngularDebounceSearch
+# Angular Debounced Search
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.7.
+## Run:
 
-## Development server
+`npm install`<br>
+`npm start`
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Open http://localhost:4200 to view it in the browser.<br>
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
 
-## Code scaffolding
+## Description
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+Angular Playground with RxJS observables. It allows you to perform instant search of articles
--- src/app/services/articles.service.spec.ts
+++ src/app/services/articles.service.spec.ts
@@ -1,27 +1,31 @@
+import { ToasterService } from './../modules/toaster/services/toaster.service';
 import { TestBed } from '@angular/core/testing';
 import { ArticlesService } from './articles.service';
 import { ArticlesResponse } from '../types/article';
 import { articlesResponseMock } from './articles.mock';
+import { of } from 'rxjs';
 
 describe('ArticlesService', () => {
   let httpClientSpy: { get: jasmine.Spy };
   let articlesService: ArticlesService;
+  let toasterService: ToasterService;
 
   beforeEach(() => {
-    // TestBed.configureTestingModule({});
-    // articlesService = TestBed.inject(ArticlesService);
     httpClientSpy = jasmine.createSpyObj('HTTPClient', ['get']);
-    articlesService = new ArticlesService(httpClientSpy as any);
+    articlesService = new ArticlesService(
+      httpClientSpy as any,
+      toasterService as any
+    );
   });
 
-  it('should return expected articles (HttpClient called once)', () => {
+  it('should return expected articles (HttpClient called once)', async () => {
     const expectedArticles: ArticlesResponse = articlesResponseMock;
-    httpClientSpy.get.and.returnValue(expectedArticles);
+    httpClientSpy.get.and.returnValue(of(expectedArticles));
     articlesService
       .getArticles('react')
       .subscribe(
-        (response) =>
-          expect(response).toEqual(expectedArticles, 'expected Articles'),
+        (heroes) =>
+          expect(heroes).toEqual(expectedArticles, 'expected articles'),
         fail
       );
     expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
